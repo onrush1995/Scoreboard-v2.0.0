7799d78b019634b7d01c148eb8076f619088ec62
Moni <moniruzzaman.eng1995@gmail.com>
2022-04-04 09:56:39 +0300

    Seperating components from app.js

--- scoreboard-v-2.0.0/src/App.js
+++ scoreboard-v-2.0.0/src/App.js
@@ -1,60 +1,6 @@
 import React from 'react';
-
-const Header = (props) => {
-    return (
-        <header>
-            <h1>{props.title}</h1>
-            <span className='stats'>Players: {props.totalPlayers + 1}</span>
-        </header>
-    );
-};
-
-class Counter extends React.Component {
-    state = {
-        score: 0
-    };
-
-    incrementScore = () => {
-        this.setState({
-            score: this.state.score + 1
-        });
-    };
-
-    decrementScore = () => {
-        this.setState({
-            score: this.state.score - 1
-        });
-    };
-
-    render() {
-        return (
-            <div className='counter'>
-                <button className='counter-action decrement' onClick={this.decrementScore}>
-                    {' '}
-                    -{' '}
-                </button>
-                <span className='counter-score'> {this.state.score} </span>
-                <button className='counter-action increment' onClick={this.incrementScore}>
-                    +
-                </button>
-            </div>
-        );
-    }
-}
-
-const Player = (props) => {
-    return (
-        <div className='player'>
-            <span className='player-name'>
-                <button className='remove-player' onClick={() => props.removePlayer(props.id)}>
-                    ✖
-                </button>
-                {props.name}
-            </span>
-            <Counter />
-        </div>
-    );
-};
+import Header from './components/Header';
+import Player from './components/Player';
 
 class App extends React.Component {
     state = {
